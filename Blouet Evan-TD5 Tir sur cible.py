66322d0a3d2d1aceca9f344731c1454196e9afaa
Evan Blouet <evan.blouet@gmail.com>
2024-04-10 16:44:12 +0200

    hopla

--- Blouet Evan-TD5 Tir sur cible.py
+++ Blouet Evan-TD5 Tir sur cible.py
@@ -1,6 +1,9 @@
 from tkinter import *
 from math import *
 from random import *
+
+#============= class
+
 class BarGUI:
     """BarGUI is simple GUI for displaying either a target or a creepy face."""
     def __init__(self):
@@ -8,10 +11,11 @@ class BarGUI:
         self.canvas = Canvas(self.root, width=400, height=400, bg='red')
         self.canvas.pack(side=TOP, padx=5, pady=5)
         self.draw_target()
-        self.but1= Button(self.root,text="Feu!!!!!!!!!!!!!!!!!!", command=self.tiraléatoire, bg="orange")
+        self.but1= Button(self.root,text="Feu!", command=self.rafalealéatoire, bg="orange")
         self.but1.pack(side=LEFT,fill=BOTH)
-        self.but2= Button(self.root,text="Ragequit", command=self.root.destroy, bg="green")
+        self.but2= Button(self.root,text="Quit", command=self.root.destroy, bg="green")
         self.but2.pack(side=RIGHT,fill=BOTH)
+        self.root.bind('f',lambda truc:self.tiraléatoire())
         self.root.mainloop()
     def draw_circle(self, x, y, r, coul1, coul2):
         """Draw un circle of centre (x, y) and radius r."""
@@ -31,17 +35,27 @@ class BarGUI:
                 self.canvas.create_text(200,45+(i-1)*30,text=str(i),font=('Times',"18",'bold'),fill='white')
             else:
                 self.canvas.create_text(200,45+(i-1)*30,text=str(i),font=('Times',"18",'bold'),fill='red')
-    def tiraléatoire(self):
+    def rafalealéatoire(self):
         S=0
         for i in range(5):
             (x,y)=(randint(0,400),randint(0,400))
-            S+=score(x,y)
-            self.draw_circle(x,y,5,'red','noir')
-        return S
-            
-BarGUI()
+            rayon=sqrt((x-200)**2+(y-200)**2)
+            touche=6-(rayon+10)//30
+            S+=max(touche, 0)
+            self.draw_circle(x,y,5,'red','black')
+        label= Label(self.root, text="Score: {} points".format(S))
+        label.pack()
+        self.but1["state"]=DISABLED
+    def tiraléatoire(self):
+        (x,y)=(randint(0,400),randint(0,400))
+        rayon=sqrt((x-200)**2+(y-200)**2)
+        touche=6-(rayon+10)//30
+        self.draw_circle(x,y,5,'red','black')
+        label= Label(self.root, text="Score: {} points".format(max(touche,0)))
+        label.pack()
+    
 
-def score(x,y):
-    rayon=sqrt((x-200)**2+(y-200)**2)
-    touche=6-rayon//30
-    return max(touche, 0)
+#============= main
+
+if __name__=='__main__':
+    BarGUI()
