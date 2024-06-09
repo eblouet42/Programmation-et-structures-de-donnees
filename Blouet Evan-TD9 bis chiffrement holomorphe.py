fcd07e3a85833f269a105b3b9db6e670996e602c
Evan Blouet <evan.blouet@gmail.com>
2024-06-09 20:38:50 +0200

    Update Blouet Evan-TD9 bis chiffrement holomorphe.py

--- Blouet Evan-TD9 bis chiffrement holomorphe.py
+++ Blouet Evan-TD9 bis chiffrement holomorphe.py
@@ -13,10 +13,10 @@ class polymod:
             newcoeff[k%n]=(newcoeff[k%n]+((-1)**(k//n)*coeff[k]))%q
         self.coeff=newcoeff
         
-    def printezlepolynomesvp(self):
+    def __str__(self):
         L=self.coeff
         if not OUESTLEPREMIERPASZERO(L)[0]:
-            print("0")
+            return("0")
         else:
             p=OUESTLEPREMIERPASZERO(L)[1]
             chaîne=""
@@ -45,10 +45,10 @@ class polymod:
                     chaîne+="X**{}".format(p)
                 else:
                     chaîne+="{}X**{}".format(L[p],p)
-            print(chaîne)
+            return(chaîne)
     
     # Exercice 2
-    def add(self,poly):
+    def __add__(self,poly):
         if self.q!=poly.q or self.n!=poly.n:
             print("euh ba c pas les mêmes n ou q")
         else:
@@ -56,7 +56,7 @@ class polymod:
             return polymod(self.q,self.n,addcoeff)
     
     # Exercice 3
-    def mul(self,poly):
+    def __mul__(self,poly):
         if self.q!=poly.q or self.n!=poly.n:
             print("euh ba c pas les mêmes n ou q")
         else:
@@ -67,12 +67,13 @@ class polymod:
             return polymod(self.q,self.n,mulcoeff)
     # Exercice 4
     def scalar(self,c):
-        scalcoeff=[(round(c*self.coeff[i]))%self.q for i in range(self.n)]
+        scalcoeff=[(c*self.coeff[i])%self.q for i in range(self.n)]
         return polymod(self.q,self.n,scalcoeff)
     def rescale(self,r):
         return polymod(r,self.n,self.coeff)
     def fscalar(self,r,alpha):
-        return self.scalar(alpha).rescale(r) 
+        fscalcoeff=[round(alpha*self.coeff[i])%r for i in range(self.n)]
+        return polymod(r,self.n,fscalcoeff).rescale(r) 
 
 #============= functions
 
@@ -89,7 +90,7 @@ def OUESTLEPREMIERPASZERO(L):
 
 # Exercice 5
 def gen_uniform_random(q,n,a,b):
-    randomcoeff=[randint(ceil(a),floor(b)+1) for i in range(n)]
+    randomcoeff=[randint(ceil(a),floor(b)) for i in range(n)]
     return polymod(q,n,randomcoeff)
 
 # Exercice 6
@@ -99,18 +100,18 @@ def chiffrement(pk,p):
     n=b.n
     t=p.q
     delta=q/t
-    sp=p.fscalar(q,delta)
+    sp=p.scalar(delta).rescale(q)
     u=gen_uniform_random(q,n,0,1)
     e1=gen_uniform_random(q,n,-1,1)
-    c1=b.mul(u).add(e1).add(sp)
+    c1=b*u+e1+sp
     e2=gen_uniform_random(q,n,-1,1)
-    c2=a.mul(u).add(e2)
+    c2=a*u+e2
     return(c1,c2)
 
 def dechiffrement(sk,c,t):
     (c1,c2)=c
     q=c1.q
-    p=(c1.add(c2.mul(sk))).fscalar(t,t/q)
+    p=(c1+c2*sk).fscalar(t,t/q)
     return p
 
 #============= main
@@ -122,54 +123,54 @@ if __name__== "__main__":
     n=4
     coeff1=[0,1,2,3,4,5]
     P1=polymod(q,n,coeff1)
-    P1.printezlepolynomesvp()
+    print(P1)
     # Exercice 2
     coeff2=[1,0,-3,0,-666,0,0,0,0,69]
     P2=polymod(q,n,coeff2)
-    P2.printezlepolynomesvp()
-    PS=P1.add(P2)
-    PS.printezlepolynomesvp()
+    print(P2)
+    PS=P1+P2
+    print(PS)
     
     # Exercice 3
-    PM=P1.mul(P2)
-    PM.printezlepolynomesvp()
+    PM=P1*P2
+    print(PM)
     
     # Exercice 4
     P2scalar=P2.scalar(3.4)
-    P2scalar.printezlepolynomesvp()
+    print(P2scalar)
     P2rescale=P2.rescale(3)
-    P2rescale.printezlepolynomesvp()
+    print(P2rescale)
     P2fscalar=P2.fscalar(3,3.4)
-    P2fscalar.printezlepolynomesvp()
+    print(P2fscalar)
     
     # Exercice 5
     random1=gen_uniform_random(10,6,42,666)
     random2=gen_uniform_random(20,6,42,666)
-    random1.printezlepolynomesvp()
-    random2.printezlepolynomesvp()
-    random1.mul(random2.rescale(random1.q)).printezlepolynomesvp()
+    print(random1)
+    print(random2)
+    print(random1*(random2.rescale(random1.q)))
     
     # Exercice 6
     grosq=97
     grosn=43
     grost=127
     coeffgrosP=[(-1)**i * i for i in range(1000)]
-    grosP=polymod(grosq,grosn,coeffgrosP)
-    grosP.printezlepolynomesvp()
+    grosP=polymod(grost,grosn,coeffgrosP)
+    print(grosP)
     sk=gen_uniform_random(grosq,grosn,0,1)
     a=gen_uniform_random(grosq,grosn,0,grosq-1)
     e=gen_uniform_random(grosq,grosn,-1,1)
-    moins1=[0 for i in range(grosn)]
-    moins1[0]=-1
-    Polmoins1=polymod(grosq,grosn,moins1)
-    b=Polmoins1.mul(a.mul(sk).add(e))
+    b=(a*sk+e).scalar(-1)
     pk=(polymod(grosq,grosn,a.coeff),polymod(grosq,grosn,b.coeff))
     chiffré=chiffrement(pk,grosP)
-    chiffré[0].printezlepolynomesvp()
-    chiffré[1].printezlepolynomesvp()
+    print(chiffré[0])
+    print(chiffré[1])
     
     # Exercice 7
     déchiffré=dechiffrement(sk,chiffré,grost)
-    déchiffré.printezlepolynomesvp()
-    assert(grosP.coeff==déchiffré.coeff)
+    print(déchiffré)
+    #assert(grosP.coeff==déchiffré.coeff)
+    coeffautregrosP=[5*i for i in range(300)]
+    autregrosP=polymod(grosq,grosn,coeffautregrosP)
+    #assert(chiffrement(pk,grosP)+chiffrement(pk,autregrosP)==chiffrement(pk,grosP+autregrosP))
     #ca marche po
